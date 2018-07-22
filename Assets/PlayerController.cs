d5c44b6cfcc986ef968fb092fac68cf0df3050b9
alexanderconner <astralbody888@gmail.com>
2018-07-22 19:56:20 -0400

    added more timelines. added more visual polish with particles. shots no longer disappear when player lets go of fire button. need to fix main timeline path for player camera.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -90,29 +90,23 @@ public class PlayerController : MonoBehaviour {
     {
         if (CrossPlatformInputManager.GetButton("Fire"))
         {
-            ActivateGuns();
+            SetGunsActive(true);
         }
         else
         {
-            DeactivateGuns();
+            SetGunsActive(false);
         }
     }
 
-    private void ActivateGuns()
+    private void SetGunsActive(bool isActive)
     {
-        foreach (GameObject gun in guns)
+        foreach (GameObject gun in guns) //may affect death fx
         {
-            gun.SetActive(true);
+            var emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
         }
     }
 
-    private void DeactivateGuns()
-    {
-        foreach (GameObject gun in guns)
-        {
-            gun.SetActive(false);
-        }
-    }
 
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ArgonAssaultRailsShooter
+
+
+
+A demo Unity 2018 rails-based flying shooter inspired by Star Fox. 
+
+
+
+
+
+
+<p align="center">
+  <img src="./screenshot1.PNG" width="350"/>  
+    </p>
+
+
+<p align="center">
+  <img src="./screenshot2.PNG" width="350"/>  
+    </p>
+
+
+<p align="center">
+  <img src="./screenshot3.PNG" width="350"/>  
+    </p>
